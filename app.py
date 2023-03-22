d6fadb9e09137b14f8c1e5d9ec21069908634283
Derek Hamersly <derek@coho.org>
2023-03-21 21:11:48 -0500

    added admin pages

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(dbname)
     
 db = SQLAlchemy(app)
 
+ADMIN_PASSWORD = 'password'
+
 class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime)
@@ -59,27 +61,26 @@ def db_init(db,start_date,numweeks=8):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    days = db.session.query(Day).order_by(Day.id).all()
     if request.method == 'POST':
-        #cook_name = request.form['content']
-        #date_clicked = '2023-03-22' #test
-        #try:
-        #    Day.query.filter(Day.date == date_clicked).update({'cook': cook_name})
-        #    db.session.commit()
-        #    return redirect('/')
-        #except:
-        #    return 'Issue adding new name to db.'
+        if request.form.get('admin_password') == ADMIN_PASSWORD:
+            return render_template('index_admin.html',days=days)
+
         return redirect('/')
     else:
-        days = db.session.query(Day).order_by(Day.id).all()
         return render_template('index.html',days=days)
 
+@app.route('/index_admin', methods=['GET'])
+def index_admin():
+    days = db.session.query(Day).order_by(Day.id).all()
+    return render_template('index_admin.html',days=days)
 
 @app.route('/update/<int:id>',methods = ['GET','POST'])
 def update(id):
     jobs = ['cook','assist','clean1','clean2']
     day_to_update = Day.query.get_or_404(id)
     if request.method == 'POST':
-        if request.form.get('click') == 'Discard Changes':
+        if request.form.get('click') == "Discard Changes/Go Back":
             return redirect('/')
         #update database with updated name
         for jobname in jobs:
@@ -102,18 +103,35 @@ def update(id):
     else:
         return render_template('update.html',day=day_to_update)
 
+@app.route('/update_admin/<int:id>',methods = ['GET','POST'])
+def update_admin(id):
+    jobs = ['cook','assist','clean1','clean2']
+    day_to_update = Day.query.get_or_404(id)
+    if request.method == 'POST':
+        if request.form.get('click') == "Discard Changes/Go Back":
+            return redirect(url_for('index_admin'))
+        #update database with updated name
+        for jobname in jobs:
+            try:
+                name_in_textbox = request.form[jobname]
+            except:
+                continue #job was not in update form because it is already assigned--see update.html jinja logic
 
-"""
-@app.route('/delete/<int:id>')
-def delete(id):
-    day_to_delete = Day.query.get_or_404(id)
-    day_to_delete.cook = 'Unassigned'
-    try:
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "Issue deleting"
-"""
+            if len(name_in_textbox.strip()) == 0: 
+                #silly goose tried to submit empty name
+                continue
+
+            exec("day_to_update.{0} = name_in_textbox".format(jobname))
+
+        try:
+            db.session.commit()
+            return redirect(url_for('index_admin'))
+        except:
+            return eval("Issue updating {0}".format(day_to_update.date))
+    else:
+        return render_template('update_admin.html',day=day_to_update)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
