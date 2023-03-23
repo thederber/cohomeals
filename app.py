f7dc5c2d53b196b4dca23cf8f9bbc93210118ce8
Derek Hamersly <derek@coho.org>
2023-03-23 01:18:43 -0500

    added login page and cleaned up code using Session variables

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template,url_for
+from flask import Flask, request, redirect, render_template,url_for,session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,timedelta
 import pandas
@@ -6,10 +6,12 @@ import pandas
 app = Flask(__name__)
 dbname = "Days"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(dbname)
-    
+app.secret_key = datetime.now().strftime('%s')
+
 db = SQLAlchemy(app)
 
 ADMIN_PASSWORD = 'password'
+COHO_PASSWORD = 'cohomeals2023'
 
 class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,32 +58,48 @@ def db_init(db,start_date,numweeks=8):
             db.session.commit()
         except:
             raise Exception('Issue adding new day')
+    
     return db
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['coho_password'] != COHO_PASSWORD:
+            error = 'Incorrect password'
+        else:
+            #assign unique username to client based on current time
+            session['username'] = 'approved'+datetime.now().strftime('%s')
+            return redirect(url_for('index'))
+    
+    return render_template('login.html',error=error)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    days = db.session.query(Day).order_by(Day.id).all()
+    error = None
+    username = session.get('username','') #'' is default if not yet signed in
+    if not username.startswith('approved'):
+        return redirect(url_for('login'))
+
     if request.method == 'POST':
-        if request.form.get('admin_password') == ADMIN_PASSWORD:
-            return render_template('index_admin.html',days=days)
-
-        return redirect('/')
-    else:
-        return render_template('index.html',days=days)
-
-@app.route('/index_admin', methods=['GET'])
-def index_admin():
+        if request.form.get('admin_password') != ADMIN_PASSWORD:
+            error = "Incorrect password"
+        else:
+            session['username'] = 'approvedadmin'+datetime.now().strftime('%s') 
+    
+    admin = session.get('username').startswith('approvedadmin')
+    print("admin = ",admin)
     days = db.session.query(Day).order_by(Day.id).all()
-    return render_template('index_admin.html',days=days)
+    return render_template('index.html',days=days,admin=admin,error=error)
 
 @app.route('/update/<int:id>',methods = ['GET','POST'])
 def update(id):
     jobs = ['cook','assist','clean1','clean2']
     day_to_update = Day.query.get_or_404(id)
+    admin = session.get('username').startswith('approvedadmin')
     if request.method == 'POST':
         if request.form.get('click') == "Discard Changes/Go Back":
-            return redirect('/')
+            return redirect(url_for('index'))
         #update database with updated name
         for jobname in jobs:
             try:
@@ -89,49 +107,24 @@ def update(id):
             except:
                 continue #job was not in update form because it is already assigned--see update.html jinja logic
 
-            if len(name_in_textbox.strip()) == 0: 
-                #silly goose tried to submit empty name
-                continue
+            if len(name_in_textbox.strip()) == 0:
+                #all whitespace--handle differently if admin or not
+                if admin:
+                    #admin was trying to delete the name
+                    name_in_textbox = 'Unassigned'
+                else:
+                    #silly goose forgot to input a name
+                    continue
 
             exec("day_to_update.{0} = name_in_textbox".format(jobname))
 
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect(url_for('index'))
         except:
             return eval("Issue updating {0}".format(day_to_update.date))
-    else:
-        return render_template('update.html',day=day_to_update)
-
-@app.route('/update_admin/<int:id>',methods = ['GET','POST'])
-def update_admin(id):
-    jobs = ['cook','assist','clean1','clean2']
-    day_to_update = Day.query.get_or_404(id)
-    if request.method == 'POST':
-        if request.form.get('click') == "Discard Changes/Go Back":
-            return redirect(url_for('index_admin'))
-        #update database with updated name
-        for jobname in jobs:
-            try:
-                name_in_textbox = request.form[jobname]
-            except:
-                continue #job was not in update form because it is already assigned--see update.html jinja logic
-
-            if len(name_in_textbox.strip()) == 0: 
-                #silly goose tried to submit empty name
-                continue
-
-            exec("day_to_update.{0} = name_in_textbox".format(jobname))
-
-        try:
-            db.session.commit()
-            return redirect(url_for('index_admin'))
-        except:
-            return eval("Issue updating {0}".format(day_to_update.date))
-    else:
-        return render_template('update_admin.html',day=day_to_update)
-
-
+    
+    return render_template('update.html',day=day_to_update,admin=admin)
 
 if __name__ == '__main__':
     app.run(debug=True)
